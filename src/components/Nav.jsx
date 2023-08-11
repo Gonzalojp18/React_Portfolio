f4be2e9c9a7e4e2743d13715173be1c4e3245197
Gonzalojp18 <pgonzalojose@gmail.com>
2023-08-11 03:13:39 -0300

    including react-route

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,12 +7,12 @@ const Nav = () => {
     return (
         <nav className='navMenu'>
             <ul className='menu'>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About Me</a></li>
-                <li><a href="/contact">Skill & tecn</a></li>
-                <li><a href="/contact">Project</a></li>
-                <li><a href="/contact">Blog</a></li>
-                <li><a href="/contact">Contact Me</a></li>
+            <li><a href="#home">Home</a></li>
+                <li><a href="#about">About Me</a></li>
+                <li><a href="#skills">Skills & Tech</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a href="#contact">Contact Me</a></li>
             </ul>
         </nav>
 
