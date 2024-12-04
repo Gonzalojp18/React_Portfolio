c0a006287c650f6889fcedcf344d646f027e02c5
Gonzalo Palomo <pgonzalojose@gmail.com>
2024-12-04 02:56:22 -0300

    chage position nav

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = ({ onNavigate, activeSection }) => {
         <motion.nav
             initial={{ y: 100 }}
             animate={{ y: 0 }}
-            className="fixed bottom-0 left-0 right-0 z-50"
+            className="fixed bottom-4 left-0 right-0 z-50"
         >
             <div className="max-w-2xl mx-auto px-4 py-2">
                 <div className="flex justify-around items-center">
